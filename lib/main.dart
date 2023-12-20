4d5291f64443eff9d18fcbb992cd530f215a7aff
majedur rahman <youremail@yourdomain.com>
2023-12-20 21:01:51 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,53 @@
-import 'package:flutter/material.dart';
-import 'package:liquid_swipe/liquid_swipe.dart';
+// import 'package:flutter/material.dart';
+// import 'package:liquid_swipe/liquid_swipe.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: "Flutter",
-    home: const HomeScreen(),
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(primarySwatch: Colors.purple),
-  ));
-}
+// void main() {
+//   runApp(MaterialApp(
+//     title: "Flutter",
+//     home: const HomeScreen(),
+//     debugShowCheckedModeBanner: false,
+//     theme: ThemeData(primarySwatch: Colors.purple),
+//   ));
+// }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
 
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
+//   @override
+//   _HomeScreenState createState() => _HomeScreenState();
+// }
 
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Card and GrirdView'),
-        ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 20,
-          children: const [
-            Card(
-              elevation: 10,
-              child: Center(child: Text("data")),
-            ),
-            Card(
-              elevation: 10,
-              child: Center(child: Text("data")),
-            ),
-            Card(
-              elevation: 10,
-              child: Center(child: Text("data")),
-            ),
-            Card(
-              elevation: 10,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10))),
-              child: Center(child: Text("data")),
-            ),
-          ],
-        ));
-  }
-}
+// class _HomeScreenState extends State<HomeScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Card and GrirdView'),
+//         ),
+//         body: GridView.count(
+//           crossAxisCount: 2,
+//           mainAxisSpacing: 10,
+//           crossAxisSpacing: 20,
+//           children: const [
+//             Card(
+//               elevation: 10,
+//               child: Center(child: Text("data")),
+//             ),
+//             Card(
+//               elevation: 10,
+//               child: Center(child: Text("data")),
+//             ),
+//             Card(
+//               elevation: 10,
+//               child: Center(child: Text("data")),
+//             ),
+//             Card(
+//               elevation: 10,
+//               shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.all(Radius.circular(10))),
+//               child: Center(child: Text("data")),
+//             ),
+//           ],
+//         ));
+//   }
+// }
