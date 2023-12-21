d39dce4310de953a8b6c6f492ca8824d2b7e63e8
majedur rahman <youremail@yourdomain.com>
2023-12-21 12:09:34 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_2024/Pages/page1.dart';
-import 'package:flutter_2024/Pages/page2.dart';
-import 'package:flutter_2024/Pages/page3.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -20,36 +17,42 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var _currentIndex = 0;
-  final pages = [
-    const Page1(),
-    const Page2(),
-    const Page3(),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: "Home",
-                backgroundColor: Colors.green),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.menu),
-                label: "menu",
-                backgroundColor: Colors.green),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.notification_add),
-                label: "menu",
-                backgroundColor: Colors.green)
-          ],
-          currentIndex: _currentIndex,
-          onTap: (index) => {setState(() => _currentIndex = index)},
+        body: CustomScrollView(
+      slivers: [
+        const SliverAppBar(
+          backgroundColor: Colors.amber,
+          expandedHeight: 150,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text("sliver app bar, list , grid"),
+            centerTitle: true,
+          ),
+          pinned: true,
         ),
-        appBar: AppBar(
-          title: const Text('BottomNavigationBar'),
-        ),
-        body: Center(child: pages[_currentIndex]));
+        SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+          return Container(
+            alignment: Alignment.center,
+            height: 50,
+            color: Colors.amber[100 * (index % 9)],
+            child: Text("Amber $index"),
+          );
+        }, childCount: 20)),
+        SliverGrid(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return Container(
+                height: 200,
+                width: 200,
+                color: Colors.amber[100 * (index % 9)],
+                alignment: Alignment.center,
+                child: Text("Amber $index"),
+              );
+            }),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, crossAxisSpacing: 20, mainAxisSpacing: 20))
+      ],
+    ));
   }
 }
