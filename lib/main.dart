ac6b88a65d3e29e411b9fbb60ef3ce3a1a8a6a09
majedur rahman <youremail@yourdomain.com>
2023-12-22 23:34:50 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var currentValue = 10.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Toast')),
+        appBar: AppBar(title: const Text('Slider')),
         body: Center(
-          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "learn flutter",
+                style: TextStyle(fontSize: currentValue),
+              ),
+              Slider(
+                activeColor: Colors.amber,
+                value: currentValue,
+                min: 10,
+                max: 50,
+                inactiveColor: Colors.red,
+                label: currentValue.toString(),
+                divisions: 5,
+                onChanged: (value) {
+                  setState(() {
+                    currentValue = value;
+                  });
+                },
+              ),
+            ],
+          ),
         ));
   }
 }
